8403dccea821ff84e6f5e8399d43ada9ae5f2f55
hashashin <hubermanandrew5@gmail.com>
2025-06-26 15:28:35 +0800

    Auto-sync: 2025-06-26 15:28:35

--- ai/ask.py
+++ ai/ask.py
@@ -1,7 +1,13 @@
+from google import genai
 from google.genai import types
+import os 
+
+client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
+
 
 with open('ai/Screenshot 2025-06-26 at 3.24.12 PM.png', 'rb') as f:
     image_bytes = f.read()
+
 
 response = client.models.generate_content(
 model='gemini-2.5-flash',
