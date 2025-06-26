143d03766e1d0d7222ad3e27d0bf526c4eb864c2
hashashin <hubermanandrew5@gmail.com>
2025-06-26 16:41:59 +0800

    Auto-sync: 2025-06-26 16:41:59

--- ai/ask.py
+++ ai/ask.py
@@ -5,19 +5,14 @@ import os
 client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
 
 
-with open('ai/Screenshot 2025-06-26 at 3.24.12 PM.png', 'rb') as f:
-    image_bytes = f.read()
-
+sample = client.files.upload(
+  file="ai/data_x_x2_x3.csv",
+  config=dict(mime_type='text/csv')
+)
 
 response = client.models.generate_content(
-model='gemini-2.5-flash',
-contents=[
-    types.Part.from_bytes(
-    data=image_bytes, 
-    mime_type='image/jpeg',
-    ),
-    'what is the name of this image '
-]
+    model="gemini-2.5-flash",
+    contents=[sample, "hwhat is this about "],
 )
 
 print(response.text)
