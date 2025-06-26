76ad084cf906be25c028fa2e6ef5d48bc0e486fa
hashashin <hubermanandrew5@gmail.com>
2025-06-26 15:38:07 +0800

    Auto-sync: 2025-06-26 15:38:07

--- ai/bulk.py
+++ ai/bulk.py
@@ -5,10 +5,11 @@ import os
 client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
 
 my_file = client.files.upload(file="ai/Screenshot 2025-06-26 at 3.24.12 PM.png")
+mon_fil_2 = client.files.upload(file="ai/Screenshot 2025-06-26 at 3.36.58 PM.png")
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=[my_file, "what is the file name of ht eimage i uploaded"],
+    contents=[my_file, mon_fil_2, "what are we looking at "],
 )
 
 print(response.text)
