7ef91c2adedf79b80c0de73ef3efb2bc7c00b882
hashashin <hubermanandrew5@gmail.com>
2025-06-28 21:47:37 +0800

    Auto-sync: 2025-06-28 21:47:37

--- development/photo2.py
+++ development/photo2.py
@@ -1,4 +1,8 @@
 import pyvista as pv
+import cadquery as cq
+
+shape = cq.importers.importStep("/Users/hashashin/Downloads/A260兼容吉利11V3.0侧视100度载具2D/qjr-aamk-02b-04lx.stp")
+cq.exporters.export(shape, 'part.stl')
 
 # --- Configuration ---
 STL_FILE = 'part.stl'
