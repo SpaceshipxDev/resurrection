cdcbdef8aa29a5cbe80d6bef7ec981c44f2f4fd6
hashashin <hubermanandrew5@gmail.com>
2025-06-28 20:17:03 +0800

    Auto-sync: 2025-06-28 20:17:03

--- development/snap.py
+++ development/snap.py
@@ -1,22 +1,14 @@
-import sys
+import cadquery as cq
+from cadquery.vis import show     # lightweight VTK viewer
 
-# Usage: freecadcmd export_stp_to_png.py input.stp output.png
+step_path = "/Users/hashashin/Downloads/A260兼容吉利11V3.0侧视100度载具2D/qjr-aamk-09b-04.stp"            # ← your STEP file
+shape     = cq.importers.importStep(step_path)   # read STEP  [oai_citation:0‡cadquery.readthedocs.io](https://cadquery.readthedocs.io/en/latest/importexport.html)
 
-infile, outfile = sys.argv[1], sys.argv[2]
-
-import FreeCAD
-import ImportGui
-
-doc = FreeCAD.newDocument()
-ImportGui.insert(infile, doc.Name)
-
-# Only works with GUI module loaded (possible in FreeCAD 0.20+ on Mac!)
-import FreeCADGui
-FreeCADGui.showMainWindow()
-view = FreeCADGui.activeDocument().activeView()
-view.viewAxonometric()
-view.fitAll()
-view.saveImage(outfile, 1024, 768, 'White')
-
-FreeCAD.closeDocument(doc.Name)
-FreeCADGui.getMainWindow().close()
+# This opens NO GUI and writes an 800×800 PNG
+show(
+    shape,
+    width=800,
+    height=800,
+    screenshot="preview.png",
+    interact=False               # headless → just render & quit
+)                                #  [oai_citation:1‡cadquery.readthedocs.io](https://cadquery.readthedocs.io/en/latest/vis.html)
