811f47d83699e9a03d92ebf9503788a319f21259
hashashin <hubermanandrew5@gmail.com>
2025-06-28 20:11:56 +0800

    Auto-sync: 2025-06-28 20:11:56

--- development/snap.py
+++ development/snap.py
@@ -1,16 +1,22 @@
 import sys
 
-# Usage: freecadcmd export_stp_to_png.py in.stp out.png
+# Usage: freecadcmd export_stp_to_png.py input.stp output.png
 
-stp_path = sys.argv[1]
-png_path = sys.argv[2]
+infile, outfile = sys.argv[1], sys.argv[2]
 
 import FreeCAD
 import ImportGui
 
 doc = FreeCAD.newDocument()
-ImportGui.insert(stp_path, doc.Name)
-view = FreeCAD.Gui.activeDocument().activeView()
+ImportGui.insert(infile, doc.Name)
+
+# Only works with GUI module loaded (possible in FreeCAD 0.20+ on Mac!)
+import FreeCADGui
+FreeCADGui.showMainWindow()
+view = FreeCADGui.activeDocument().activeView()
 view.viewAxonometric()
 view.fitAll()
-view.saveImage(png_path, 800, 600, 'White')
+view.saveImage(outfile, 1024, 768, 'White')
+
+FreeCAD.closeDocument(doc.Name)
+FreeCADGui.getMainWindow().close()
