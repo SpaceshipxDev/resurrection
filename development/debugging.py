63dc6b780077ad00b53ad594ed13efb1bb0fded8
hashashin <hubermanandrew5@gmail.com>
2025-07-01 11:22:46 +0800

    Auto-sync: 2025-07-01 11:22:46

--- development/debugging.py
+++ development/debugging.py
@@ -1,7 +1,11 @@
 import pyvista as pv
 
-mesh = pv.read('development/part.stl')
-plotter = pv.Plotter()
-plotter.add_mesh(mesh, color='tan', show_edges=True)
-plotter.camera_position = 'iso'
-plotter.show()
+STL_FILE= "/Users/hashashin/Documents/grokkk/generated_stl/a260-jl11v-aazj-01.stl"
+OUTPUT_IMAGE="printed.png"
+plotter = pv.Plotter(off_screen=True) 
+mesh = pv.read(STL_FILE) 
+plotter.add_mesh(mesh, color="tan", show_edges=False) 
+plotter.camera_position="iso" 
+plotter.screenshot(OUTPUT_IMAGE, window_size=[1920, 1080])
+
+plotter.close() 
