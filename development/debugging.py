cdde555ad97bd3e129b3079571169b89fdf26210
hashashin <hubermanandrew5@gmail.com>
2025-07-01 11:32:12 +0800

    Auto-sync: 2025-07-01 11:32:12

--- development/debugging.py
+++ development/debugging.py
@@ -1,6 +1,6 @@
 import pyvista as pv
 
-STL_FILE= "Users/hashashin/Documents/grokkk/generated_stl/a260-jl11v-aazj-01.stl"
+STL_FILE= "/Users/hashashin/Documents/grokkk/generated_stl/a260-jl11v-aazj-01.stl"
 OUTPUT_IMAGE="printed.png"
 plotter = pv.Plotter(off_screen=True) 
 mesh = pv.read(STL_FILE) 
