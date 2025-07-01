39935735015a44a9036ba62e81c3a2f2cfb843b1
hashashin <hubermanandrew5@gmail.com>
2025-07-01 11:19:29 +0800

    Auto-sync: 2025-07-01 11:19:29

--- development/debugging.py
+++ development/debugging.py
@@ -1,6 +1,6 @@
 import pyvista as pv
 
-mesh = pv.read('/Users/hashashin/Documents/grokkk/generated_stl/a260-jl11v-aazj-01.stl')
+mesh = pv.read('development/part.stl')
 plotter = pv.Plotter()
 plotter.add_mesh(mesh, color='tan', show_edges=True)
 plotter.camera_position = 'iso'
